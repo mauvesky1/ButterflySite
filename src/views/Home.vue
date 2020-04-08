8a9714d8917db3cb7ff29b1d0b6a2b2ff434eed5
conwayhub <ofdermott@gmail.com>
2020-04-08 14:59:01 +0100

    added a very simple login form and account creation, as well as a button to toggle between them

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
     <HelloWorld msg="Butterfly House 2020" />
+    <LoginForm v-if="!creatingAccount" />
+    <CreateAccountForm v-if="creatingAccount" />
+    <button
+      v-if="!creatingAccount"
+      v-on:click="creatingAccount = !creatingAccount"
+    >Create a New Account</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import LoginForm from "@/components/LoginForm.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import CreateAccountForm from "@/components/CreateAccountForm.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginForm,
+    CreateAccountForm
+  },
+  data: function() {
+    return { creatingAccount: false };
   }
 };
 </script>
