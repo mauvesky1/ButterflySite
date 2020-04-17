7ae972b8628ea922d2f68e215b10f106b273e954
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-04-17 11:34:43 +0100

    write a get request to firebase for the butterflies in collection.vue

--- src/components/ParentProfile.vue
+++ src/components/ParentProfile.vue
@@ -3,11 +3,7 @@
     <Header />
     <h1 class="welcome-msg">Welcome {{ $route.params.parentusername }}</h1>
 
-    <p
-      v-for="child in childrenUsers"
-      :key="child.username"
-      class="child-username"
-    >
+    <p v-for="child in childrenUsers" :key="child.username" class="child-username">
       <router-link
         :to="{ name: 'ChildProfile', params: { username: child.username } }"
         class="child-link"
@@ -31,22 +27,22 @@ export default {
   name: "ParentProfile",
   components: {
     Header,
-    CreateChildProfile,
+    CreateChildProfile
   },
   data() {
     return {
-      childrenUsers: [],
+      childrenUsers: []
     };
   },
   created() {
     firestore()
       .collection(`parents/${window.localStorage.parentDoc}/userProfiles`)
       .get()
-      .then((children) => {
-        children.docs.forEach((child) => {
+      .then(children => {
+        children.docs.forEach(child => {
           this.childrenUsers.push({
             username: child.lm.Ee.proto.mapValue.fields.username.stringValue,
-            avatarUrl: child.lm.Ee.proto.mapValue.fields.avatarUrl.stringValue,
+            avatarUrl: child.lm.Ee.proto.mapValue.fields.avatarUrl.stringValue
           });
         });
       });
@@ -54,8 +50,8 @@ export default {
   methods: {
     addNewChild(newChildUser) {
       this.childrenUsers = [...this.childrenUsers, newChildUser];
-    },
-  },
+    }
+  }
 };
 </script>
 
