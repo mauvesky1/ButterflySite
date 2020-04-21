3956622b4cf7e2bf809c7e7ef48ab6de257df4d1
bob <rob.draper@live.com>
2020-04-21 18:28:14 +0100

    history

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import  from './router'
 import './firebase'
 import VueRouterBackButton from 'vue-router-back-button'
 
 Vue.config.productionTip = false
 Vue.use(VueRouterBackButton, { router })
 
+
+
 new Vue({
+  history: true,
   router,
   render: h => h(App)
 }).$mount('#app')
